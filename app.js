3ef95387a1d5b611436600b1e15e9172cc502397
PY-GZKY <341796767@qq.com>
2022-01-24 14:19:32 +0800

    增加页面下拉刷新事件(2022-01-24)

--- app.js
+++ app.js
@@ -9,6 +9,31 @@ App({
     this.slides();
   },
 
+  //刷新
+  // onRefresh() {
+  //   //在当前页面显示导航条加载动画
+  //   wx.showNavigationBarLoading();
+  //   //显示 loading 提示框。需主动调用 wx.hideLoading 才能关闭提示框
+  //   wx.showLoading({
+  //     title: '刷新中'
+  //   });
+  //   //隐藏loading 提示框
+  //   wx.hideLoading();
+  //   //隐藏导航条加载动画
+  //   wx.hideNavigationBarLoading();
+  //   //停止下拉刷新
+  //   wx.stopPullDownRefresh();
+  // },
+
+  // /**
+  //  * 页面相关事件处理函数--监听用户下拉动作
+  //  */
+  // onPullDownRefresh: function () {
+  //   //调用刷新时将执行的方法
+  //   console.log("开始刷新！！");
+  //   this.onRefresh();
+  // },
+
   slides() {
     wx.request({
       url: 'https://tplan.cc/slides',
